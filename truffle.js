711e7ae0f4f7fc5a82fb089fbd7e8360aeba4eb1
alfredov@garena.com <alfredov@garena.com>
2020-12-11 20:56:21 +0700

    edit truffle.js

--- truffle.js
+++ truffle.js
@@ -1,15 +1,22 @@
 module.exports = {
   networks: {
-    development: {
+    develop: {
       host: "127.0.0.1",
-      port: 8545,
-      network_id: "*" // Match any network id
+      port: 7545,
+      // network_id: "5777",
+      // port: 8545,
+      network_id: "*", // Match any network id
     }
-    
+  },
+  ganache: {
+    host: "localhost",
+    port: 8545,
+    gas: 5000000,
+    network_id: "*"
   },
   compilers: {
     solc: {
-      version: "0.4.24", 
+      version: "0.5.1", 
     }
   }
 };
