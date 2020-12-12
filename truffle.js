61988779055819ff8eba91d83c0e80cb17b49f2f
alfredov@garena.com <alfredov@garena.com>
2020-12-12 22:55:13 +0700

    add infura to truffle

--- truffle.js
+++ truffle.js
@@ -1,7 +1,7 @@
 
 
 const HDWallet = require('truffle-hdwallet-provider');
-const infuraKey = "https://ropsten.infura.io/v3/edc52c91d64f4201b4c1c87f89d2283a";
+const infuraKey = "https://ropsten.infura.io/v3/3a52a0ac2bcb432791a1e8365ccfb0d5";
 //
 const fs = require('fs');
 const mnemonic = fs.readFileSync(".secret").toString().trim();
@@ -14,29 +14,30 @@ module.exports = {
       // network_id: "5777",
       // port: 8545,
       network_id: "*", // Match any network id
-    }
+    },
+    ganache: {
+      host: "localhost",
+      port: 8545,
+      gas: 5000000,
+      network_id: "*"
+    },
+     // Another network with more advanced options...
+     rinkeby: {
+      provider: () => new HDWallet(mnemonic, infuraKey),
+      network_id: 4,
+      gas : 6700000,
+      gasPrice : 10000000000
+    },
+  
+    // Useful for deploying to a public network.
+    // NB: It's important to wrap the provider as a function.
+    ropsten: {
+      provider: () => new HDWallet(mnemonic, infuraKey),
+      network_id: 3,       // Ropsten's id,
+      gas: 4000000,        // Ropsten has a lower block limit than mainnet
+    },
   },
-  ganache: {
-    host: "localhost",
-    port: 8545,
-    gas: 5000000,
-    network_id: "*"
-  },
-   // Another network with more advanced options...
-   rinkeby: {
-    provider: () => new HDWallet(mnemonic, infuraKey),
-    network_id: 4,
-    gas : 6700000,
-    gasPrice : 10000000000
-  },
-
-  // Useful for deploying to a public network.
-  // NB: It's important to wrap the provider as a function.
-  ropsten: {
-    provider: () => new HDWallet(mnemonic, infuraKey),
-    network_id: 3,       // Ropsten's id,
-    gas: 4000000,        // Ropsten has a lower block limit than mainnet
-  },
+  
 
   compilers: {
     solc: {
