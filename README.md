33c4fced2368395926017183f40a1a6b712667af
Vidy <vidy@pop-os.localdomain>
2020-12-22 10:17:32 +0700

    1. update transaction hash in README.md 2. update configuration of truffle.js

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 
 # Supply Chain
-    - I use ropsten network as 
 
-## Contract on Ropsten
+## Contract on ropsten
                         
     Transaction Hash: 0x29329c265b499f2f1b87d2f765159fd2b19d91a21ec3a4736c9e33826db9bba1
-    Contract address: 0xc5f5394aaBA480503c4832B10657d155b1FBC571
+    Contract address: 0xd1a86383caff95f076d371c21472628ee5ab8448
                         
 ## How to run:
     ```
--- truffle.js
+++ truffle.js
@@ -1,16 +1,15 @@
-
-
-const HDWallet = require('truffle-hdwallet-provider');
-const infuraKey = "https://ropsten.infura.io/v3/3a52a0ac2bcb432791a1e8365ccfb0d5";
+const HDWallet = require("truffle-hdwallet-provider");
+const infuraKey =
+  "https://ropsten.infura.io/v3/3a52a0ac2bcb432791a1e8365ccfb0d5";
 //
-const fs = require('fs');
+const fs = require("fs");
 const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   networks: {
     develop: {
       host: "127.0.0.1",
-      port: 7545,
+      port: 8545,
       // network_id: "5777",
       // port: 8545,
       network_id: "*", // Match any network id
@@ -19,30 +18,30 @@ module.exports = {
       host: "localhost",
       port: 8545,
       gas: 5000000,
-      network_id: "*"
+      network_id: "*",
     },
-     // Another network with more advanced options...
-     rinkeby: {
+    // Another network with more advanced options...
+    rinkeby: {
       provider: () => new HDWallet(mnemonic, infuraKey),
       network_id: 4,
-      gas : 6700000,
-      gasPrice : 10000000000
+      gas: 6700000,
+      gasPrice: 10000,
     },
-  
+
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     ropsten: {
       port: 8545,
       provider: () => new HDWallet(mnemonic, infuraKey),
-      network_id: 3,       // Ropsten's id,
-      gas: 1000000,        // Ropsten has a lower block limit than mainnet
+      network_id: 3, // Ropsten's id,
+      gas: 6700000,
+      gasPrice: 10000,
     },
   },
-  
 
   compilers: {
     solc: {
-      version: "0.4.26", 
-    }
-  }
+      version: "0.4.26",
+    },
+  },
 };
